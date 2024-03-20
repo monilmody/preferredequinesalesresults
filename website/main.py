5dd212fae7a656d4ad74ef0e9bf7180344b6f8c2
Monil Mody <40249034+monilmody@users.noreply.github.com>
2024-03-20 09:09:40 -0400

    Update main.py

--- website/main.py
+++ website/main.py
@@ -108,7 +108,7 @@ def upload_data_to_mysql(df):
             DAMOFDAM = Column(String(50))
             CDAMOFDAM = Column(String(50))
             DAMTATT = Column(String(6))
-            DAMYOF = Column(Integer, nullable=True)
+            DAMYOF = Column(String(50))
             DDAMTATT = Column(String(6))
             tsales = relationship("Tsales", back_populates="tdamsire")
 
