a787238a5cca5c4c4a09a54a8f2c6defb8c05ebf
monilmody <monilmody9@gmail.com>
2024-04-18 12:38:10 -0400

    hip range to populate saledate and day values

--- website/main.py
+++ website/main.py
@@ -1969,7 +1969,7 @@ def obs():
 
         df.drop(columns=['foaling_year'], inplace=True)
 
-        df['UTT'] = df['ut_time'].fillna("")
+        df['UTT'] = df['ut_time'].fillna(0.0)
         
         df.drop(columns=['ut_time'], inplace=True)
 
